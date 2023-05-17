152f415b63c87b3df377648a7f8c89a3e3b6ddda
Omar Al Hendi <omar.hendi@rihal.om>
2023-05-18 00:49:31 +0800

    fix(web): add a barchart for malaysia population

--- pages/charts.tsx
+++ pages/charts.tsx
@@ -1,83 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
-  BarChart,
-  Bar,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
+  BarChart,
+  Bar,
   ResponsiveContainer,
 } from "recharts";
 import { fetcher } from "@/utils";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const charts = ({ data: formData }: any) => {
+  const [data, setData] = useState([]);
 
-const charts = ({ data: what }: any) => {
-  console.log(what);
+  useEffect(() => {
+    const filteredData = formData.filter(
+      (d: any) => d.state !== "Malaysia" && !!d.state
+    );
+
+    setData(filteredData);
+  }, [formData]);
+
   return (
     <div>
-      <h1>Charts Page</h1>
+      <h1 style={{ margin: "4rem" }}>Line Charts For Malaysia Population</h1>
       <ResponsiveContainer width="100%" height={400}>
-        <BarChart
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
+        <BarChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <XAxis dataKey="state" interval={0} />
+          <YAxis type="number" />
           <Tooltip />
           <Legend />
-          <Bar dataKey="name" fill="#8884d8" />
-          <Bar dataKey="priceUsd" fill="#82ca9d" />
+          <Bar dataKey="pop_5" fill="#8884d8" />
+          <Bar dataKey="pop_12" fill="#82ca9d" />
+          <Bar dataKey="pop_18" fill="#8884d8" />
+          <Bar dataKey="pop_60" fill="#82ca9d" />
         </BarChart>
       </ResponsiveContainer>
     </div>
