6749b0b88e0dba71eb170fb39569fae03d07e790
Omar Al Hendi <omar.hendi@rihal.om>
2023-05-20 14:35:12 +0800

    fix(web): install carbon charts

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,8 +1,26 @@
-import { LoaderFunction } from "react-router-dom";
-import axios from "axios";
+import { fetcher } from "../utils";
 
-export const homeLoader = (async (): Promise<any> => {
-  const response = await fetch("/api");
-  const message: Promise<any> = response.json();
-  return message;
-}) satisfies LoaderFunction;
+export const feedbackLoader = async () => {
+  const vaccinationData = await fetcher("vaccination/vax_state.csv");
+  const populationData = await fetcher("static/population.csv");
+
+  const filteredVaccinationData = vaccinationData
+    .slice(-17)
+    .filter((row: any) => !!row.state);
+  const filteredPopulationdata = populationData.filter(
+    (row: any) => !!row.state
+  );
+
+  let columns = filteredVaccinationData.map((row: any) => {
+    const state: any = filteredPopulationdata.find(
+      ({ state }: any) => state === row.state
+    );
+    return {
+      vaccination: +row["cumul_full"],
+      state: row["state"],
+      percentage: (+row["cumul_full"] / +state["pop"]) * 100,
+    };
+  });
+
+  return { data: columns };
+};
