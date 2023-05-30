6458d292dff54b60cdef136840da0c014fbaec69
Lok17 <rotabite987@gmail.com>
2023-05-30 21:33:07 +0800

    fix:use carbon and show label for value larger than 500,000

--- src/pages/TreeMapPage.tsx
+++ src/pages/TreeMapPage.tsx
@@ -2,6 +2,8 @@ import { treeMapLoader } from "../loaders";
 import { useLoaderData } from "react-router-dom";
 import { LoaderData } from "../types";
 import { TreemapChart } from "@carbon/charts-react";
+import "@carbon/styles/css/styles.css";
+import "@carbon/charts/styles.css";
 
 const options = {
   title: "Treemap(daily partial of vaccination by districts)",
