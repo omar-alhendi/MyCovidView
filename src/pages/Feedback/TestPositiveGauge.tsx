03cea24afe61e0bbde49f374b75057d35ef0485e
cheekingneoh <cheeking8919@gmail.com>
2023-06-03 11:26:51 +0800

    Clean up commits

--- src/pages/Feedback/TestPositiveGauge.tsx
+++ src/pages/Feedback/TestPositiveGauge.tsx
@@ -2,19 +2,19 @@ import { GaugeChart } from "@carbon/charts-react";
 import "@carbon/charts/styles.css";
 import { GaugeTypes, Statuses } from "@carbon/charts/interfaces";
 
+const options = {
+    "title": "Gauge (Covid-19 Test Positve Rate)",
+    "resizable": true,
+    "height": "250px",
+    "width": "100%",
+    "gauge": {
+      "type": GaugeTypes.SEMI,
+      "status": Statuses.DANGER
+    }
+}
+
 
 const TestPositiveGauge = ({ data }: { data: any })=>{
-    const options = {
-        "title": "Gauge (Covid-19 Test Positve Rate)",
-        "resizable": true,
-        "height": "250px",
-        "width": "100%",
-        "gauge": {
-          "type": GaugeTypes.SEMI,
-          "status": Statuses.DANGER
-        }
-    }
-
     return (
         <div>
             <h1>Positive Rate for Covid Tests</h1>
