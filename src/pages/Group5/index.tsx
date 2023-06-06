442e009db237637ba996a60c638114a4f7f7cef3
kyuuurina <17204442@siswa.um.edu.my>
2023-06-07 01:07:06 +0800

    added donut chart into group-5 page

--- src/pages/Group5/index.tsx
+++ src/pages/Group5/index.tsx
@@ -1,23 +1,26 @@
-import VaccinatedPeople from "./VaccinatedPeople";
-import { useLoaderData } from "react-router-dom";
-import { LoaderData } from "../../types";
-import { group5Loader } from "../../loaders";
+import VaccinatedPeople from './VaccinatedPeople';
+import VaccinatedPeopleByDistrict from './VaccinatedPeopleByDistrict';
+import { useLoaderData } from 'react-router-dom';
+import { LoaderData } from '../../types';
+import { group5Loader } from '../../loaders';
 
 const LollipopChartPage = () => {
-  const {lollipopData} =
-    useLoaderData() as LoaderData<typeof group5Loader>;
+  const { lollipopData, donutData } = useLoaderData() as LoaderData<
+    typeof group5Loader
+  >;
   return (
     <div
       style={{
-        display: "grid",
-        gap: "2rem",
-        background: "#e0e0e0",
-        padding: "2rem",
-        borderRadius: "1rem",
-        margin: "2rem",
+        display: 'grid',
+        gap: '2rem',
+        background: '#e0e0e0',
+        padding: '2rem',
+        borderRadius: '1rem',
+        margin: '2rem',
       }}
     >
       <VaccinatedPeople data={lollipopData} />
+      <VaccinatedPeopleByDistrict data={donutData} />
     </div>
   );
 };
