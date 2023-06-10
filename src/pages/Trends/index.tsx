46c10328810eb142bb13e2a3beec4abd8c4f26c1
wannazreena <waneenaahmed@gmail.com>
2023-06-11 02:38:54 +0800

    Removed unrelated charts from trend identification page

--- src/pages/Trends/index.tsx
+++ src/pages/Trends/index.tsx
@@ -1,29 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import { LoaderData } from "../../types";
 import { trendsLoader } from "../../loaders";
-import StackedLine from "./StackedLine";
 import "../../styles/chart-bg.css";
-import StackedArea from "./StackedArea";
-import Combo from "./Combo";
 
 // line, area, scatter plots
 const TrendsPage = () => {
   const {
-    stackedLineData,
-    stackedAreaData,
-    comboData
   } = useLoaderData() as LoaderData<typeof trendsLoader>;
 
   return (
     <div>
       <div className="chart-bg">
-        <StackedLine data={stackedLineData} />
-      </div>
-      <div className="chart-bg">
-        <StackedArea data={stackedAreaData} />
-      </div>
-      <div className="chart-bg">
-        <Combo data={comboData} />
       </div>
     </div>
   );
