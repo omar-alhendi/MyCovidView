b93c82ee91462a8fed8c32dff608c3bc1d67522d
Omar Al Hendi <98800354+omar-al-hendi@users.noreply.github.com>
2023-06-12 04:50:41 +0800

    Revert "Add some styles"
    This reverts commit 8f39ca8041d19c71a575ec69c20c67501af4f1dc.

--- src/pages/GroupGalaxy/index.tsx
+++ src/pages/GroupGalaxy/index.tsx
@@ -12,11 +12,10 @@ const GroupGalaxyPage = () => {
         style={{
           display: "grid",
           gap: "2rem",
-          background: "#b4d0e7",
+          background: "#e0e0e0",
           padding: "2rem",
-          borderRadius: "2rem",
+          borderRadius: "1rem",
           margin: "2rem",
-          boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.2)",
         }}
       >
         <ColumnChart data={columnChartData} />
