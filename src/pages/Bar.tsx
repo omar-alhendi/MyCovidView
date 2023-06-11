e6baba57e6a41b2b5ab0aa28926f2e69eb062624
Omar Al Hendi <98800354+omar-al-hendi@users.noreply.github.com>
2023-06-12 04:50:41 +0800

    Revert "fix(web): remove old Bar chart"
    This reverts commit 3df936061c0d014c369613db7c327fee09ade8da.

---
+++ src/pages/Bar.tsx
@@ -0,0 +1,49 @@
+import { SimpleBarChart } from "@carbon/charts-react";
+import "@carbon/charts/styles.css";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+import { ScaleTypes } from "@carbon/charts/interfaces";
+import "@carbon/charts/styles.css";
+import "@carbon/styles/css/styles.css";
+import { LoaderData } from "../types";
+import { simpleBarLoader } from "../loaders";
+
+const options = {
+	"title": "Total Death in Each State",
+	"axes": {
+		"left": {
+			"mapsTo": "group",
+			"title": "State",
+			"scaleType": ScaleTypes.LABELS,
+		},
+		"bottom": {
+			"title": "Total Death",
+			"mapsTo": "value"
+		}
+	},
+	"height": "400px",
+}
+
+const BarChart = () => {
+  const data = useLoaderData() as LoaderData<typeof simpleBarLoader>;
+
+	const {sortOrder = ""} = useParams();
+	const navigate = useNavigate();
+
+	return (
+		<div>
+			<SimpleBarChart
+				data={data}
+				options={options} />
+			<select value={sortOrder} onChange={(event) => {
+				navigate(`/bar/${event.target.value}`)
+			}}>
+				<option value="">None</option>
+				<option value="asc">Ascending</option>
+				<option value="desc">Descending</option>
+			</select>
+		</div>
+	);
+}
+
+export default BarChart;
+  
