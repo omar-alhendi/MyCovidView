de2d80393fc07f00f455e2e362190c28e3a7b107
Omar Al Hendi <omar.hendi@rihal.om>
2023-06-12 00:47:07 +0800

    fix(web): fix the charts loading bug

--- src/loaders/fantasy/index.ts
+++ src/loaders/fantasy/index.ts
@@ -1,9 +1,9 @@
 import { fetcher } from "../../utils";
-  
-  export const lineChartLoader = async () => {
-    const data = await fetcher("epidemic/cases_malaysia.csv");
-    
-    const lineChartData = data
+
+export const lineChartLoader = async () => {
+  const data = await fetcher("epidemic/cases_malaysia.csv");
+
+  const lineChartData = data
     .map((row: any) => {
       const { date, cases_active } = row;
 
@@ -25,50 +25,49 @@ import { fetcher } from "../../utils";
     .flat();
 
   return lineChartData;
-  }
-  
-  export const areaChartLoader = async () => {
-    let vaccinationData: any = await fetcher("vaccination/vax_malaysia.csv");
-    vaccinationData.pop();
-    vaccinationData = vaccinationData.slice(-30);
+};
 
-    const groups = ["Booster 1", "Booster 2"];
-    const dailyKeys = [
-      "daily_booster",
-      "daily_booster2",
-    ];
+export const areaChartLoader = async () => {
+  let vaccinationData: any = await fetcher("vaccination/vax_malaysia.csv");
+  vaccinationData.pop();
+  vaccinationData = vaccinationData.slice(-30);
 
-    const data = vaccinationData.flatMap((row: any) =>
-      groups.map((group, index) => ({
-        group,
-        date: row["date"],
-        value: row[dailyKeys[index]],
-      }))
+  const groups = ["Booster 1", "Booster 2"];
+  const dailyKeys = ["daily_booster", "daily_booster2"];
+
+  const data = vaccinationData.flatMap((row: any) =>
+    groups.map((group, index) => ({
+      group,
+      date: row["date"],
+      value: row[dailyKeys[index]],
+    }))
+  );
+
+  console.log(data, "from the arechart");
+
+  return data;
+};
+
+export const scatterPlotLoader = async () => {
+  const vaccinationData = await fetcher("vaccination/vax_state.csv");
+  const populationData = await fetcher("static/population.csv");
+
+  const filteredVaccinationData = vaccinationData
+    .slice(-17)
+    .filter((row: any) => !!row.state);
+  const filteredPopulationdata = populationData.filter(
+    (row: any) => !!row.state
+  );
+
+  const data = filteredVaccinationData.map((row: any) => {
+    const state: any = filteredPopulationdata.find(
+      ({ state }: any) => state === row.state
     );
-  
-    return data;
-  }
+    return {
+      group: row["state"],
+      value: (+row["cumul_full"] / +state["pop"]) * 100,
+    };
+  });
 
-  export const scatterPlotLoader = async () => {
-    const vaccinationData = await fetcher("vaccination/vax_state.csv");
-    const populationData = await fetcher("static/population.csv");
-  
-    const filteredVaccinationData = vaccinationData
-      .slice(-17)
-      .filter((row: any) => !!row.state);
-    const filteredPopulationdata = populationData.filter(
-      (row: any) => !!row.state
-    );
-  
-    const data = filteredVaccinationData.map((row: any) => {
-      const state: any = filteredPopulationdata.find(
-        ({ state }: any) => state === row.state
-      );
-      return {
-        group: row["state"],
-        value: (+row["cumul_full"] / +state["pop"]) * 100,
-      };
-    });
-  
-    return data;
-  }
+  return data;
+};
