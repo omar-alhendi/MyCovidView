fa10fa14e762cb523b9bedeede60cbda018c4d86
kyuuurina <17204442@siswa.um.edu.my>
2023-06-14 22:43:52 +0800

    added heatmap and fantasy scatter plot to exploration page

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -1,8 +1,8 @@
-import { Outlet, Link, useLocation } from "react-router-dom";
-import { SideNav, SideNavItems } from "carbon-components-react";
-import "@carbon/styles/css/styles.css";
-import "@carbon/charts/styles.css";
-import "./Container.css";
+import { Outlet, Link, useLocation } from 'react-router-dom';
+import { SideNav, SideNavItems } from 'carbon-components-react';
+import '@carbon/styles/css/styles.css';
+import '@carbon/charts/styles.css';
+import './Container.css';
 
 interface Link {
   to: string;
@@ -10,26 +10,27 @@ interface Link {
 }
 
 const links: Link[] = [
-  { to: "/", label: "Overview" },
-  { to: "/feedback", label: "Feedback" },
-  { to: "/impact", label: "Impact" },
-  { to: "/group-2", label: "Group 2" },
-  { to: "/group-5", label: "Group 5" },
-  { to: "/group-11", label: "Group 11" },
-  { to: "/comparison", label: "Comparison" },
-  { to: "/trend-identification", label: "Trend Identification" },
-  { to: "/correlation", label: "Correlation" },
-  { to: "/distribution", label: "Distribution" },
-  { to: "/group-13", label: "Group 13" },
-  { to: "/dendrogram", label: "Dendrogram" },
-  { to: "/heatmap", label: "Heatmap" },
-  { to: "/import", label: "Import" },
-  { to: "/group-galaxy", label: "Group Galaxy" },
-  { to: "/fantasy", label: "Fantasy" },
-  { to: "/patterns", label: "Pattern" },
-  { to: "/ranking", label: "Ranking" },
-  { to: "/proportion", label: "Proportion" },
-  { to: "/composition", label: "Composition" },
+  { to: '/', label: 'Overview' },
+  { to: '/feedback', label: 'Feedback' },
+  { to: '/impact', label: 'Impact' },
+  { to: '/group-2', label: 'Group 2' },
+  { to: '/group-5', label: 'Group 5' },
+  { to: '/group-11', label: 'Group 11' },
+  { to: '/group-13', label: 'Group 13' },
+  { to: '/comparison', label: 'Comparison' },
+  { to: '/trend-identification', label: 'Trend Identification' },
+  { to: '/correlation', label: 'Correlation' },
+  { to: '/distribution', label: 'Distribution' },
+  { to: '/dendrogram', label: 'Dendrogram' },
+  { to: '/heatmap', label: 'Heatmap' },
+  { to: '/import', label: 'Import' },
+  { to: '/group-galaxy', label: 'Group Galaxy' },
+  { to: '/fantasy', label: 'Fantasy' },
+  { to: '/patterns', label: 'Pattern' },
+  { to: '/ranking', label: 'Ranking' },
+  { to: '/proportion', label: 'Proportion' },
+  { to: '/composition', label: 'Composition' },
+  { to: '/exploration', label: 'Exploration' },
 ];
 
 const Container = () => {
@@ -46,7 +47,7 @@ const Container = () => {
                 key={link.to}
                 to={link.to}
                 className={`sidebar-link ${
-                  pathname === link.to ? "active" : ""
+                  pathname === link.to ? 'active' : ''
                 }`}
               >
                 {link.label}
---
+++ src/pages/Exploration/index.tsx
@@ -0,0 +1,30 @@
+import { useLoaderData } from 'react-router-dom';
+import { LoaderData } from '../../types';
+import { explorationLoader } from '../../loaders';
+import ScatterPlotPage from './ScatterPlotPage';
+import HeatMap from './HeatMap';
+
+const ExplorationPage = () => {
+  const { scatterPlotData, heatMapData } = useLoaderData() as LoaderData<
+    typeof explorationLoader
+  >;
+  return (
+    <div>
+      <div
+        style={{
+          display: 'grid',
+          gap: '2rem',
+          background: '#e0e0e0',
+          padding: '2rem',
+          borderRadius: '1rem',
+          margin: '2rem',
+        }}
+      >
+        <ScatterPlotPage data={scatterPlotData} />
+        <HeatMap data={heatMapData} />
+      </div>
+    </div>
+  );
+};
+
+export default ExplorationPage;
