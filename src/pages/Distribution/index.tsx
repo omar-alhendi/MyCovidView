f6d8cd3f83a5bba6eb411af90d7b5757861cc748
sarahatiqah <sarahatiqahamram@gmail.com>
2023-06-19 10:53:58 +0800

    Add Histogram Chart to Distribution page

--- src/pages/Distribution/index.tsx
+++ src/pages/Distribution/index.tsx
@@ -3,12 +3,14 @@ import { LoaderData } from "../../types";
 import { distributionLoader } from "../../loaders";
 import BoxPlot from "./BoxPlot.tsx";
 import Donut from "./Donut.tsx";
+import Histogram from "./Histogram.tsx";
 import "../../styles/chart-bg.css";
 
 const DistributionPage = () => {
   const {
     boxPlotData,
-    donutData
+    donutData,
+    histogramData
   } = useLoaderData() as LoaderData<typeof distributionLoader>;
 
   return (
@@ -18,6 +20,9 @@ const DistributionPage = () => {
       </div>
       <div className="chart-bg">
         <Donut data={donutData} />
+      </div>
+      <div className="chart-bg">
+        <Histogram data={histogramData} />
       </div>
     </div>
   );
