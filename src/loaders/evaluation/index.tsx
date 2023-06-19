c06f9defd40b234025b81279a1cfaa0763429c8e
sarahatiqah <sarahatiqahamram@gmail.com>
2023-06-19 11:04:28 +0800

    Added Balance Board to Evaluation page

--- src/loaders/evaluation/index.tsx
+++ src/loaders/evaluation/index.tsx
@@ -39,3 +39,49 @@ export const casesLoader = async () => {
   };
   return data;
 };
+
+export const testsLoader = async () => {
+  let OriginalData = await fetcher("epidemic/tests_malaysia.csv");
+  const xArr: string[] = [];
+  const pcr: number[] = [];
+  const rtkag: number[] = [];
+  OriginalData = OriginalData.filter((item: any) => {
+    const itemDate: string = item.date;
+    const itemDateArr = itemDate.split("-");
+    const itemYear: number = Number(itemDateArr[0]);
+    const itemMonth: number = Number(itemDateArr[1]);
+    const itemDay: number = Number(itemDateArr[2]);
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth: number = currentDate.getMonth() + 1;
+
+    const methods = ["years", "month"];
+    switch (methods[0]) {
+      case "years":
+        if (itemYear === currentYear) {
+          xArr.push(itemDate);
+          pcr.push(item["pcr"]);
+          rtkag.push(item["rtk-ag"]);
+        }
+        return itemYear === currentYear;
+      case "month":
+        if (itemYear === currentYear && itemMonth === currentMonth) {
+          xArr.push(itemDate);
+          pcr.push(item["pcr"]);
+          rtkag.push(item["rtk-ag"]);
+        }
+        return itemYear === currentYear && itemMonth === currentMonth;
+      default:
+        break;
+    }
+  });
+  const data = {
+    xAxis: xArr,
+    Pcr: pcr,
+    Rtkag: rtkag,
+  };
+  console.log(data);
+
+  return data;
+};
