be2fd540631c7b56977b5b00085cb255a357d500
qinlh <2394416759@qq.com>
2023-06-28 12:31:45 +0800

    fix: kpi full screen name

--- src/pages/Evaluation/KPIDashboard.tsx
+++ src/pages/Evaluation/KPIDashboard.tsx
@@ -90,41 +90,41 @@ export default function KPIDashboard({ data }: { data: any }) {
                         },
 
                         contentToOption: function () {
-                          const workbook = XLSX.utils.book_new();
-                          const ws1 = XLSX.utils.table_to_sheet(document.getElementById("dataTable"));
+                            const workbook = XLSX.utils.book_new();
+                            const ws1 = XLSX.utils.table_to_sheet(document.getElementById("dataTable"));
                             XLSX.utils.book_append_sheet(workbook, ws1, "Sheet1");
                             XLSX.writeFile(workbook, "Cases.xlsx");
                         },
                     },
                     myFull: { // 全屏
                         show: true,
-                        title: '全屏',
+                        title: 'Full Screen',
                         icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
                         onclick: () => {
                             const element = document.getElementById('charts');
-                            if(!element){
-                              console.log("element is null");
-                              return;
-                            } 
+                            if (!element) {
+                                console.log("element is null");
+                                return;
+                            }
                             // 全屏查看
                             if (element.requestFullscreen) { // HTML W3C 提议
                                 element.requestFullscreen()
                             } else if ((element as any).msRequestFullscreen) { // IE11
-                              (element as any).msRequestFullScreen()
+                                (element as any).msRequestFullScreen()
                             } else if ((element as any).webkitRequestFullScreen) { // Webkit
-                              (element as any).webkitRequestFullScreen()
+                                (element as any).webkitRequestFullScreen()
                             } else if ((element as any).mozRequestFullScreen) { // Firefox
-                              (element as any).mozRequestFullScreen()
+                                (element as any).mozRequestFullScreen()
                             }
                             // 退出全屏
                             if ((element as any).msRequestFullScreen) {
-                              (document as any).msExitFullscreen()
+                                (document as any).msExitFullscreen()
                             } else if ((element as any).webkitRequestFullScreen) {
-                              (document as any).webkitCancelFullScreen()
+                                (document as any).webkitCancelFullScreen()
                             } else if ((element as any).mozRequestFullScreen) {
-                              (document as any).mozCancelFullScreen()
+                                (document as any).mozCancelFullScreen()
                             } else {
-                              document.exitFullscreen()
+                                document.exitFullscreen()
                             }
                         }
                     }
@@ -136,7 +136,7 @@ export default function KPIDashboard({ data }: { data: any }) {
         <>
             <div>
                 <h1>KPI Dashboard</h1>
-                </div>
+            </div>
             <div id='charts' className='kpiCharts' style={{ width: '90%', margin: '0 auto' }}>
                 <EChartsReact style={{ marginTop: 100 }
                 } option={option} ></EChartsReact >
